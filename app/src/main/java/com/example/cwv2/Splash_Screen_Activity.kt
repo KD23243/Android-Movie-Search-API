1c285b8cc6edfafdaedf007048a7203e8878bf15
KD23243 <Kumudithag40@gmail.com>
2022-04-11 09:21:49 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/example/cwv2/Splash_Screen_Activity.kt
@@ -0,0 +1,25 @@
+package com.example.cwv2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+class Splash_Screen_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@Splash_Screen_Activity, MainActivity::class.java);
+            startActivity(intent)
+            overridePendingTransition(
+                com.google.android.material.R.anim.abc_fade_in,
+                com.google.android.material.R.anim.abc_fade_out
+            )
+        }, 3000)
+    }
+}
